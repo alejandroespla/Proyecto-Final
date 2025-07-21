ddc3c470dac7e610a26ee8827a9232f32781631d
Espla <37451532+alejandroespla@users.noreply.github.com>
2025-07-21 18:51:41 +0000

    front login

--- src/front/pages/Login.jsx
+++ src/front/pages/Login.jsx
@@ -1,27 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import { Link } from "react-router-dom";
 
-
-
-export const Home = () => {
+export const Login = () => {
 	const { store, dispatch } = useGlobalReducer();
+
+	const [backgroundUrl, setBackgroundUrl] = useState("");
+
+	const backgrounds = [
+		"https://static.nationalgeographic.es/files/styles/image_3200/public/gettyimages-1272468011.webp?w=1600&h=1068",
+		"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEPT9_GDJ1885ZHKbBP0AYr1dCFIcGJF9o8w&s",
+		"https://res.cloudinary.com/aenetworks/image/upload/c_fill,ar_2,w_3840,h_1920,g_auto/dpr_auto/f_auto/q_auto:eco/v1/whoinventedfootball?_a=BAVAZGDX0",
+		
+	];
+
+	useEffect(() => {
+		const randomIndex = Math.floor(Math.random() * backgrounds.length);
+		setBackgroundUrl(backgrounds[randomIndex]);
+	}, []);
+
+	const currentBackground = {
+		backgroundImage: `url(${backgroundUrl})`,
+		backgroundSize: "cover",
+		backgroundPosition: "center",
+		minHeight: "100vh"
+	};
 
 	const handleLogin = (e) => {
 		e.preventDefault();
 		const email = e.target.email.value;
 		const password = e.target.password.value;
 
-		// Aquí podrías llamar a una acción de login o API
 		console.log("Iniciando sesión con:", { email, password });
-
-		// Ejemplo: dispatch({ type: "LOGIN", payload: { email, password } });
 	};
 
 	return (
-		<div className="container d-flex justify-content-center p-5">
-			<div className="card shadow-sm p-4" style={{ maxWidth: "400px", width: "100%" }}>
-				<h3 className="text-center mb-4">Login</h3>
+		<div style={currentBackground} className="d-flex justify-content-start p-5">
+			<div className="d-flex align-content-center card shadow-sm p-5 bg-white bg-opacity-75" style={{ maxWidth: "600px", width: "100%", borderRadius:"24px"}}>
+				<img className="mx-auto d-block mb-3"  
+					 src="../src/front/assets/img/logo.png" 
+					 style={{ maxWidth: "280px", width: "100%", borderRadius:"24px"}} 
+					 alt="Logo"
+				/>
+				<h2 className="text-center mb-4">Login</h2>
 				<form onSubmit={handleLogin}>
 					<div className="mb-3">
 						<label htmlFor="email" className="form-label">Email</label>
@@ -32,17 +53,16 @@ export const Home = () => {
 						<input type="password" className="form-control" id="password" name="password" placeholder="••••••••" required />
 					</div>
 					<div className="d-grid mb-3">
-						<button type="submit" className="btn btn-primary">login</button>
+						<button type="submit" className="btn" style={{ backgroundColor: "#2E676A", border: "none", borderRadius: "8px", color:"#ffffff" }}>Login</button>
 					</div>
 				</form>
 				<div className="text-center">
-					<small className="me-1">Don´t have an account?</small>
+					<small className="me-1">Don’t have an account?</small>
 					<Link to={"/register"}>
 						<small>Register here</small>
 					</Link>
-
 				</div>
 			</div>
-		</div >
+		</div>
 	);
 };
