f7a89aa690cf295a67860c40a215c58f7e5293df
Marlon Pons Blanco <mapachestropicales@gmail.com>
2025-07-22 20:40:15 +0000

    Finalizado el front del registro

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -9,7 +9,8 @@ import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
-import { Register } from "./pages/Register";
+import Register from "./pages/Register";
+
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -20,6 +21,8 @@ export const router = createBrowserRouter(
     // Note: The child paths of the Layout element replace the Outlet component with the elements contained in the "element" attribute of these child paths.
 
       // Root Route: All navigation will start from here.
+      <>
+      
       <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
 
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
@@ -28,5 +31,6 @@ export const router = createBrowserRouter(
         <Route path="/demo" element={<Demo />} />
         <Route path="/register" element={<Register />} />
       </Route>
+      </>
     )
 );
