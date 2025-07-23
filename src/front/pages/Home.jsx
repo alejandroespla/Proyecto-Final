4e6be9870000a4d773b439b6b0c9b3ead51653b4
GJoao24 <122391147+GJoao24@users.noreply.github.com>
2025-07-23 16:54:45 +0000

    Navbar Front End

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -2,8 +2,9 @@ import React, { useEffect } from "react"
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
-export const Home = () => {
 
+export const Home = () => {
+/*
 	const { store, dispatch } = useGlobalReducer()
 
 	const loadMessage = async () => {
@@ -31,22 +32,10 @@ export const Home = () => {
 	useEffect(() => {
 		loadMessage()
 	}, [])
+*/
 
 	return (
-		<div className="text-center mt-5">
-			<h1 className="display-4">Hello Rigo!!</h1>
-			<p className="lead">
-				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
-			</p>
-			<div className="alert alert-info">
-				{store.message ? (
-					<span>{store.message}</span>
-				) : (
-					<span className="text-danger">
-						Loading message from the backend (make sure your python 🐍 backend is running)...
-					</span>
-				)}
-			</div>
-		</div>
+		
+		<h2>Prueba</h2>
 	);
 }; 
--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -8,7 +8,7 @@ import {
 import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
-import { Demo } from "./pages/Demo";
+
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -24,7 +24,6 @@ export const router = createBrowserRouter(
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
         <Route path= "/" element={<Home />} />
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
-        <Route path="/demo" element={<Demo />} />
       </Route>
     )
 );
